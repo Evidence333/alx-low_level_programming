4a0c2d3400da9d9830066db01f814bc4b47ff93c
root <Evidence333>
2023-02-24 06:14:00 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_number(void);
+void print_most_number(void);
+void more_number(void);
+void print_line(void);
+void print_diagona(int n);
+void print_square(int size);
+void print_trangle(int size);
+#endif
